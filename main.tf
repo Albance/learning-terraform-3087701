f865e1aadaabdd07f6cfc7762a31ea4b08ead72d
Albance <Vengercevas@gmail.com>
2025-04-06 15:13:21 +0100

    autoscaling complete

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-########################
-# Data Sources
-########################
-
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -18,123 +14,79 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-########################
-# VPC
-########################
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs            = data.aws_availability_zones.available.names
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
     Environment = "dev"
   }
 }
 
-########################
-# Security Group
-########################
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-
-  vpc_id = module.blog_vpc.vpc_id
-  name   = "blog"
-
-  ingress_rules       = ["http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-########################
-# Auto Scaling Group
-########################
-
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.2.0"
+  version = "6.5.2"
 
-  name               = "blog"
-  min_size           = 1
-  max_size           = 2
-  desired_capacity   = 1
+  name = "blog"
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  create_launch_template = true
-  launch_template_name   = "blog-launch-template"
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name        = "blog-asg"
-    Environment = "dev"
-  }
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
-
-########################
-# Application Load Balancer
-########################
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "9.15.0"
+  version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  name = "blog-alb"
 
-  listeners = {
-    ex-http = {
-      port     = 80
-      protocol = "HTTP"
+  load_balancer_type = "application"
 
-      forward = {
-        target_group_key = "ex-instance"
-      }
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
     }
-  }
+  ]
 
-  target_groups = {
-    ex-instance = {
-      name_prefix = "blog"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
   }
-
-  depends_on = [module.autoscaling]
 }
 
-########################
-# ASG → ALB Attachment
-########################
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-resource "aws_autoscaling_attachment" "asg_attachment" {
-  autoscaling_group_name = module.autoscaling.autoscaling_group_name
-  target_group_arn        = module.blog_alb.target_groups["ex-instance"].arn
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
